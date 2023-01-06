462536505b91f1791e65467f6d074a0db4e48c4b
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2023-01-05 21:02:13 -0500

    Complete balancer

--- balance-docstring/src/App.jsx
+++ balance-docstring/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import DocstringInput from './docstringInput'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <DocstringInput></DocstringInput>
+    <div className="container mx-auto p-4">
+      <h1 className="text-xl font-sans w-fit mx-auto mb-2 font-bold">🐍 Docstring Balancer</h1>
+      <p className="italic w-fit mx-auto mb-3">
+        Format docstrings to a specified line length, preserving paragraph breaks.
+        <br/>
+        Note: This doesn't handle indents gracefully (yet!). Unindent then re-indent after. 
+      </p>
+      <DocstringInput className="w-100"></DocstringInput>
     </div>
   )
 }
--- balance-docstring/src/docstringInput.jsx
+++ balance-docstring/src/docstringInput.jsx
@@ -2,58 +2,91 @@ import React, {useState} from 'react';
 
 function DocstringInput() {
   const parse = (length, inputText) => {
-    console.log(inputText)
-    const re = new RegExp(`(?:[\\s\\S]{0,${length - 2}}\\S\\b)|(?:\\S*?\\b)`, "g")
-    console.log(re.toString())
-    const lines = inputText.match(re) || []
-    console.log(lines)
-    return lines.join("\n")
+    const re = new RegExp(`\\S[\\s\\S]{0,${length-1}}(?=(?:\\s|$))|\\S[\\s\\S]{${length-1}}\\S+`, "g")
+    // split paragraphs
+    const paragraphs = inputText.split(/\n\s*\n/)
+    const parsedParagraphs = paragraphs.map((paragraph) => {
+      const lines = paragraph.replace("\n", " ").match(re) || []
+      return lines.filter((line) => !!line).join("\n")
+    })
+    return parsedParagraphs.join("\n\n")
   }
   
 
   const [inputField, setInputField] = useState("")
   const [numChars, setNumChars] = useState(100)
+  const [tmpNumChars, setTmpNumChars] = useState(100)
 
   const inputFieldChange = (event) => {
     setInputField(event.target.value);
   }
   
-  const numCharsChange = (event) => {
-    let num = Math.max(3, event.target.value)
-    setNumChars(num);
+  const tmpNumCharsChange = (event) => {
+    setTmpNumChars(event.target.value)
+    console.log(numChars, tmpNumChars)
   }
 
+  const handleNumCharsConfirm = () => {
+    let num = Math.max(2, tmpNumChars)
+    setNumChars(num)
+    setTmpNumChars(num)
+    console.log(numChars, tmpNumChars)
+  }
+
+  let outputField = parse(numChars, inputField)
+
   return (
-      <div>
-          <textarea 
+      <div class="mx-auto w-fit">
+          <textarea
           type="text" 
           name="inputDocstring" 
+          cols="100"
+          rows="10"
           onChange={inputFieldChange} 
           placeholder='""""your docstring..."""'
-          style={{FontFace: "monospace"}}
-          cols="100"
-          rows="25"
-          value={inputField}/>           
+          className="font-mono p-4 rounded-md"
+          value={inputField}/>
+          <div className="relative w-fit h-fit">
+            <textarea
+            type="text"
+            name="outputDocstring"
+            className="font-mono p-4 rounded-md"
+            disabled
+            cols="100"
+            rows="10"
+            placeholder='"""...nicely formatted"""'
+            value={outputField}></textarea>
+            <button
+            onClick={() => {navigator.clipboard.writeText(outputField)}}
+            className="absolute top-2 right-2 bg-gray-600 p-1 rounded-md"
+            aria-label="copy" 
+            title="copy"
+            >
+              📋
+            </button>
+          </div>
           <br/>
 
-          <textarea 
-          type="text" 
-          name="outputDocstring" 
-          cols="100"
-          rows="25"
-          disabled
-          placeholder='"""...nicely formatted"""' 
-          value={parse(numChars, inputField)}/>
-
-          <br/>
-
-          <input
-          type="number"
-          name="numChars"
-          onChange={numCharsChange}
-          placeholder="ex: 100"
-          value={numChars}
-          />
+          <div className="h-fit inline-block">
+            <div class="p">Max Chars per Line</div>
+            <input
+            type="number"
+            name="numChars"
+            onInput={tmpNumCharsChange}
+            className="font-mono p-2 rounded-l-md h-100"
+            placeholder="ex: 100"
+            min="3"
+            value={tmpNumChars}
+            />
+            <button
+            className="h-100 bg-gray-600 p-2 rounded-r-md"
+            aria-label="confirm number of characters" 
+            onClick={handleNumCharsConfirm}
+            title="submit"
+            >
+              ✅
+            </button>
+          </div>
       </div>  
   )
 }
